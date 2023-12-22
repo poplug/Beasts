53863bce60c8d1baec1c499bccfcb002c8c1f95f
poplug <po.the.plug@proton.me>
2023-12-22 14:41:49 +1100

    Initial upload

---
+++ Api/PoeNinja.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Beasts.Api;
+
+public static class PoeNinja
+{
+    private static readonly string PoeNinjaUrl = "https://poe.ninja/api/data/itemoverview?league=Affliction&type=Beast";
+
+    private class PoeNinjaLine
+    {
+        [JsonProperty("name")] public string Name;
+        [JsonProperty("chaosValue")] public float ChaosValue;
+    }
+
+    private class PoeNinjaResponse
+    {
+        [JsonProperty("lines")] public List<PoeNinjaLine> Lines;
+    }
+
+    public static async Task<Dictionary<string, float>> GetBeastsPrices()
+    {
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(PoeNinjaUrl);
+        if (!response.IsSuccessStatusCode) throw new HttpRequestException("Failed to get poe.ninja response");
+
+        var json = await response.Content.ReadAsStringAsync();
+        var poeNinjaResponse = JsonConvert.DeserializeObject<PoeNinjaResponse>(json);
+
+        return poeNinjaResponse.Lines.ToDictionary(line => line.Name, line => line.ChaosValue);
+    }
+}
---
+++ Beasts.Render.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Beasts.Data;
+using Beasts.ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.Shared.Enums;
+using ImGuiNET;
+using SharpDX;
+using Vector2 = System.Numerics.Vector2;
+using Vector3 = System.Numerics.Vector3;
+
+namespace Beasts;
+
+public partial class Beasts
+{
+    public override void Render()
+    {
+        foreach (var positioned in _trackedBeasts
+                     .Select(beast => beast.Value.GetComponent<Positioned>())
+                     .Where(positioned => positioned != null))
+        {
+            DrawFilledCircleInWorldPosition(
+                GameController.IngameState.Data.ToWorldWithTerrainHeight(positioned.GridPosition), 50
+            );
+        }
+
+        DrawBestiaryPanel();
+        DrawBeastsWindow();
+    }
+
+    private void DrawBestiaryPanel()
+    {
+        var bestiary = GameController.IngameState.IngameUi.GetBestiaryPanel();
+        if (bestiary == null || bestiary.IsVisible == false) return;
+
+        var capturedBeastsPanel = bestiary.CapturedBeastsPanel;
+        if (capturedBeastsPanel == null || capturedBeastsPanel.IsVisible == false) return;
+
+        var beasts = bestiary.CapturedBeastsPanel.CapturedBeasts;
+        foreach (var beast in beasts)
+        {
+            var beastMetadata = BeastsDatabase.AllBeasts.Find(b => b.DisplayName == beast.DisplayName);
+            if (beastMetadata == null) continue;
+            if (!Prices.ContainsKey(beastMetadata.DisplayName)) continue;
+
+            var center = new Vector2(beast.GetClientRect().Center.X, beast.GetClientRect().Center.Y);
+
+            Graphics.DrawBox(beast.GetClientRect(), new Color(0, 0, 0, 0.5f));
+            Graphics.DrawFrame(beast.GetClientRect(), Color.White, 2);
+            Graphics.DrawText(beastMetadata.DisplayName, center, Color.White, FontAlign.Center);
+
+            var text = Prices[beastMetadata.DisplayName].ToString(CultureInfo.InvariantCulture);
+            var textPos = center + new Vector2(0, 20);
+            Graphics.DrawText(text, textPos, Color.White, FontAlign.Center);
+        }
+    }
+
+    private void DrawBeastsWindow()
+    {
+        ImGui.SetNextWindowSize(new Vector2(0, 0));
+        ImGui.SetNextWindowBgAlpha(0.6f);
+        ImGui.Begin("Beasts Window", ImGuiWindowFlags.NoDecoration);
+
+        if (ImGui.BeginTable("Beasts Table", 3, ImGuiTableFlags.RowBg | ImGuiTableFlags.BordersOuter | ImGuiTableFlags.BordersV))
+        {
+            ImGui.TableSetupColumn("Price", ImGuiTableColumnFlags.WidthFixed, 48);
+            ImGui.TableSetupColumn("Beast");
+
+            foreach (var beastMetadata in _trackedBeasts
+                         .Select(trackedBeast => trackedBeast.Value)
+                         .Select(beast => BeastsDatabase.AllBeasts.Find(b => b.Path == beast.Metadata))
+                         .Where(beastMetadata => beastMetadata != null))
+            {
+                ImGui.TableNextRow();
+
+                ImGui.TableNextColumn();
+
+                ImGui.Text(Prices.TryGetValue(beastMetadata.DisplayName, out var price)
+                    ? $"{price.ToString(CultureInfo.InvariantCulture)}c"
+                    : "0c");
+
+                ImGui.TableNextColumn();
+
+                ImGui.Text(beastMetadata.DisplayName);
+                foreach (var craft in beastMetadata.Crafts)
+                {
+                    ImGui.Text(craft);
+                }
+            }
+
+            ImGui.EndTable();
+        }
+
+        ImGui.End();
+    }
+
+    private void DrawFilledCircleInWorldPosition(Vector3 position, float radius)
+    {
+        var circlePoints = new List<Vector2>();
+        const int segments = 15;
+        const float segmentAngle = 2f * MathF.PI / segments;
+
+        for (var i = 0; i < segments; i++)
+        {
+            var angle = i * segmentAngle;
+            var currentOffset = new Vector2(MathF.Cos(angle), MathF.Sin(angle)) * radius;
+            var nextOffset = new Vector2(MathF.Cos(angle + segmentAngle), MathF.Sin(angle + segmentAngle)) * radius;
+
+            var currentWorldPos = position + new Vector3(currentOffset, 0);
+            var nextWorldPos = position + new Vector3(nextOffset, 0);
+
+            circlePoints.Add(GameController.Game.IngameState.Camera.WorldToScreen(currentWorldPos));
+            circlePoints.Add(GameController.Game.IngameState.Camera.WorldToScreen(nextWorldPos));
+        }
+
+        Graphics.DrawConvexPolyFilled(circlePoints.ToArray(),
+            Color.Red with { A = Color.ToByte((int)((double)0.2f * byte.MaxValue)) });
+        Graphics.DrawPolyLine(circlePoints.ToArray(), Color.Red, 2);
+    }
+}
