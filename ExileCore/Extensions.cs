f408eea09d62d371051da64de7496559a8e693dc
poplug <po.the.plug@proton.me>
2024-04-29 13:30:30 +1000

    Fix Bestiary Panel for 3.24

--- ExileCore/Extensions.cs
+++ ExileCore/Extensions.cs
@@ -7,12 +7,12 @@ public static class Extensions
     public static BestiaryPanel GetBestiaryPanel(this IngameUIElements ui)
     {
         return ui.GetObject<BestiaryPanel>(
-            ui.ChallengesPanel
+            ui.GetChildAtIndex(46)
                 ?.GetChildAtIndex(2)
                 ?.GetChildAtIndex(0)
                 ?.GetChildAtIndex(1)
                 ?.GetChildAtIndex(1)
-                ?.GetChildAtIndex(60).Address ?? 0
+                ?.GetChildAtIndex(62).Address ?? 0
         );
     }
 }
